af905da635b246cec2bedfcac1bf81eab424e681
S <shakibmohammad18@gmail.com>
2024-09-12 22:21:36 +0600

    google map implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:belives_store_app_app/views/pages/bottomNavBar/bottom_nav_bar.dart';
 import 'package:belives_store_app_app/views/pages/category/cate.dart';
-import 'package:belives_store_app_app/views/pages/category/categoryes.dart';
-import 'package:belives_store_app_app/views/pages/order_review/order_review.dart';
-import 'package:belives_store_app_app/views/pages/shipping/shipping_address.dart';
-import 'package:belives_store_app_app/views/pages/shipping_new_address/google_maps.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
